b6daec2b1df6a24de1c6ffaadb2caced16f29d3d
Claudio Caletti <claudio@buildo.io>
2017-03-02 08:53:24 +0100

    Add logback test implementation

---
+++ loggoLogback/src/main/scala/Boot.scala
@@ -0,0 +1,56 @@
+package loggo
+
+import org.slf4j.LoggerFactory
+import ch.qos.logback.core.util.StatusPrinter
+import ch.qos.logback.classic.LoggerContext
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import slick.jdbc.H2Profile.api._
+
+object Models {
+  case class User(
+    id: Int,
+    username: String
+  )
+
+  class Users(tag: Tag) extends Table[User](tag, "users") {
+    def id = column[Int]("id", O.PrimaryKey)
+    def name = column[String]("name")
+    def * = (id, name) <> (User.tupled, User.unapply)
+  }
+
+  object Users extends TableQuery[Users](new Users(_))
+}
+
+object Boot extends App {
+  import scala.concurrent.Await
+  import scala.concurrent.duration._
+
+  val logger = LoggerFactory.getLogger(this.getClass)
+
+  import Models._
+
+  val timeout = 10 seconds
+
+  val db = Database.forConfig("h2mem1")
+
+  try {
+    val setup = DBIO.seq(
+      Users.schema.create,
+      Users ++= Seq(
+        User(1, "Gabro"),
+        User(2, "Dani")
+      )
+    )
+
+    Await.result(db.run(setup), timeout)
+
+    val futureUser = db.run(Users.result)
+
+    val user = Await.result(futureUser, timeout)
+
+    logger.info("this is an info log")
+    logger.error("erroro", user)
+  } finally db.close
+}
